8abcf5eff91fddb70713c160323567c320bbd758
LitvinovVN <LitvinovVN@rambler.ru>
2018-08-08 18:18:12 +0300

    created empty projects

---
+++ ElasticityClassLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ElasticityClassLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ ElasticityConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using ElasticityClassLibrary;
+using System;
+
+namespace ElasticityConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine($"Hello {class1}!");
+        }
+    }
+}
